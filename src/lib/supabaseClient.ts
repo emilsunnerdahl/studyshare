851072c988e20d0648b5553d35326df1e20d3175
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-10-06 14:56:42 +0200

    .env

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://qiunaulinmhrdtvgpqjj.supabase.co";
-const supabasePublishableKey = "sb_publishable_FTIFF1x8P0dLAgDymcbuSQ_SQBYcwYu";
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabasePublishableKey =import.meta.env.SUPABASE_PUBLISHABLE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey);
