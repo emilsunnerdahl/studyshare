cb961264d90c748fa78527e104aa4154dda52a55
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-10-10 10:38:44 +0200

    new color design

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,12 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 export const Button = ({ children, className = "", ...props }: ButtonProps) => {
     return (
         <button
-            className={`px-4 py-2 rounded bg-blue-600 text-white hover:bg-blue-700 transition cursor-pointer ${className}`}
+            className={`inline-flex items-center justify-center px-4 py-2
+                 rounded-md border border-blue-700
+                 bg-white text-blue-800
+                 hover:bg-blue-50 active:bg-blue-100
+                 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-600 focus-visible:ring-offset-2
+                 shadow-sm transition-colors cursor-pointer ${className}`}
             {...props}
         >
             {children}
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -69,7 +69,14 @@ const Landing = () => {
             </section>
 
             {/* 🚀 Call to Action Section */}
-            <section className="w-full px-6 py-16 bg-gradient-to-r from-indigo-600 to-purple-600 text-white text-center space-y-6 rounded-4xl">
+            <section className="
+                w-full 
+                px-6 
+                py-16 
+                bg-[linear-gradient(90deg,rgba(191,207,219,1)_0%,rgba(59,101,217,1)_44%,rgba(52,90,207,1)_61%,rgba(89,126,235,1)_100%)]
+                text-white text-center 
+                space-y-6 rounded-4xl"
+            >
                 <h2 className="text-2xl sm:text-3xl font-semibold">
                     {t("readyToStart")}
                 </h2>
