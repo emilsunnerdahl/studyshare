1df434bc724aaa357a3454ce3c6edd0051f71754
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-10-29 10:14:43 +0100

    organise into components - landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,62 +1,25 @@
 import { useTranslation } from "react-i18next";
-import { Button } from "../components/Button";
 import { useNavigate } from "react-router-dom";
-import InfoCard from "../components/InfoCard";
 import TestimonialCard from "../components/TestimonialCard";
 import { useAuth } from "@/hooks/useAuth";
 import CTA from "../components/landing/cta";
+import Hero from "../components/landing/hero";
+import HowItWorks from "@/components/landing/howItWorks";
 
 const Landing = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
   const { user } = useAuth();
 
-  const handleClick = () => {
-    const programCode = localStorage.getItem("program_code");
-    if (programCode) {
-      navigate(`/programs/${programCode}`);
-      return;
-    }
-    navigate("/programs");
-  };
-
   return (
     <>
       <main className="flex flex-col items-center">
-        <section
-          className="w-full 
-                 bg-[linear-gradient(45deg,rgba(191,217,255,1)_0%,rgba(245,247,250,1)_44%,rgba(191,217,255,1)_80%)]
-                 px-6 sm:px-8 md:px-12 py-20 text-center space-y-6 rounded-4xl"
-        >
-          <h1 className="text-4xl sm:text-5xl font-extrabold tracking-tight text-gray-900">
-            {t("landingTitle")}
-          </h1>
-          <p className="text-lg text-gray-700 max-w-2xl mx-auto">
-            {t("landingDesc")}
-          </p>
-          <div className="flex flex-col sm:flex-row gap-4 justify-center">
-            <Button onClick={() => handleClick()}>{t("courses")}</Button>
-            {!user && (
-              <Button onClick={() => navigate("/auth")}>{t("signIn")}</Button>
-            )}
-          </div>
-        </section>
+        <Hero />
 
-        <section className="w-full max-w-6xl px-6 py-20 bg-white space-y-10 rounded-4xl mt-10 mb-20">
-          <h2 className="text-2xl sm:text-3xl font-bold text-center">
-            {t("howItWorksTitle")}
-          </h2>
-          <div className="flex flex-col md:flex-row gap-6">
-            <InfoCard
-              title={t("browseCourses")}
-              description={t("browseDesc")}
-            />
-            <InfoCard title={t("writeReviews")} description={t("writeDesc")} />
-            <InfoCard title={t("saveCourses")} description={t("saveDesc")} />
-          </div>
-        </section>
+        <HowItWorks />
 
-        <section className="w-full max-w-6xl px-6 py-20 bg-gray-50 space-y-10 rounded-4xl mb-20">
+        {/* När vi har testimonials, lägg in här */}
+        {/* <section className="w-full max-w-6xl px-6 py-20 bg-gray-50 space-y-10 rounded-4xl mb-20">
           <h2 className="text-2xl sm:text-3xl font-bold text-center">
             {t("testimonialsTitle")}
           </h2>
@@ -65,7 +28,7 @@ const Landing = () => {
             <TestimonialCard quote="Lorem ipsum" author="x, LTH" />
             <TestimonialCard quote="Lorem ipsum" author="x, LTH" />
           </div>
-        </section>
+        </section> */}
 
         {!user && <CTA />}
       </main>
