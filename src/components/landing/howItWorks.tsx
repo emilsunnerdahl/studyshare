2acadc4af637a0ced4733ccca4a736427fbaf405
Emil Sunnerdahl <emil.sunnerdahl@gmail.com>
2025-11-09 19:58:37 +0100

    icons to info cards

--- src/components/landing/howItWorks.tsx
+++ src/components/landing/howItWorks.tsx
@@ -1,22 +1,33 @@
 import { useTranslation } from "react-i18next";
 import InfoCard from "../InfoCard";
+import { Heart } from "lucide-react";
+import { Compass, Edit3, Bookmark } from "lucide-react";
 
 export default function HowItWorks() {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <section className="w-full max-w-6xl px-6 py-20 bg-white space-y-10 rounded-4xl mt-10 mb-20">
-            <h2 className="text-2xl sm:text-3xl font-bold text-center">
-                {t("howItWorksTitle")}
-            </h2>
-            <div className="flex flex-col md:flex-row gap-6">
-                <InfoCard
-                    title={t("browseCourses")}
-                    description={t("browseDesc")}
-                />
-                <InfoCard title={t("writeReviews")} description={t("writeDesc")} />
-                <InfoCard title={t("saveCourses")} description={t("saveDesc")} />
-            </div>
-        </section>
-    )
+  return (
+    <section className="w-full max-w-6xl px-6 py-10 sm:py-20 bg-white space-y-10 rounded-4xl sm:mt-10 sm:mb-10">
+      <h2 className="text-2xl sm:text-3xl font-bold text-center">
+        {t("howItWorksTitle")}
+      </h2>
+      <div className="flex flex-col md:flex-row gap-6">
+        <InfoCard
+          title={t("browseCourses")}
+          description={t("browseDesc")}
+          Icon={Compass}
+        />
+        <InfoCard
+          title={t("writeReviews")}
+          description={t("writeDesc")}
+          Icon={Edit3}
+        />
+        <InfoCard
+          title={t("saveCourses")}
+          description={t("saveDesc")}
+          Icon={Bookmark}
+        />
+      </div>
+    </section>
+  );
 }
